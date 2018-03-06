3041dd28ef7d349b986b25fb74df1d7b512f2742
thanndieu <thann.dieu@gmail.com>
2018-03-06 16:49:26 +0700

    BUoi1-Bai1

---
+++ Buoi1-Bai1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Enumerate IP addresses
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                                  ipaddress.ToString());
+            }
+        }
+    }
+}
