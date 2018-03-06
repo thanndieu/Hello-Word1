a4ca15200b069563027e77f40b9408d6f4da24cf
thanndieu <thann.dieu@gmail.com>
2018-03-06 17:35:33 +0700

    BUoi1-Bai1

---
+++ Buoi1-Bai2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public void netResolve()
+        {
+           
+        }
+            
+
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao ten Host: ");
+            string host = Console.ReadLine();
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Ten: " + host);
+            foreach (IPAddress ip in Dns.GetHostAddresses("google.com"))
+            {
+                Console.WriteLine(ip.ToString());
+            }
+        }
+    }
+}
